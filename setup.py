4f28d267b9f043b3625534a1b0df9dacf4050aa9
Chill Guy 3:00PM <khoigaming2102pro@gmail.com>
2026-03-11 20:34:25 +0700

    Bump version from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="devpulse-tool",
-    version="0.2.0",
+    version="0.2.1",
     description="DevPulse - AI powered code scanner and developer productivity tool",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
